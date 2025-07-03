c1c18fe76b533a3594115584608fd8d37e6374ae
Namit <pareeknamit8@gmail.com>
2025-07-03 22:25:34 +0530

    Deploying to Github Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    base: '/weather-app/'
-  ],
+  plugins: [tailwindcss()],
+  base: '/weather-app/'  // ✅ base goes here, NOT inside plugins
 })
